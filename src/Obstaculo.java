03d3bf8f37e3285083a22a4cbc0828eec8e4675b
daniag56 <dnlrsgrc@gmail.com>
2026-02-17 09:16:27 +0100

    Porteado

--- src/Obstaculo.java
+++
@@ -1,8 +0,0 @@
-public class Obstaculo extends Jugador {
-
-    public Obstaculo(int fila, int columna) {
-        super(fila, columna, "#");
-    }
-
-   
-}
--- src/Roguelike.java
+++
@@ -1,6 +0,0 @@
-public class Roguelike {
-    public static void main(String[] args) throws Exception {
-        Juego.iniciarJuego();
-    }
-}
-
